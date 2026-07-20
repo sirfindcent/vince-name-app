90d7fd9120f79670db14b7b6639aadeb12b55204
Vincent <vinc.jonathan@gmail.com>
2026-07-20 15:30:34 -0500

    feat(api): support query parameter

--- backend/src/test/java/com/vince/backend/controller/GreetingControllerTest.java
+++ backend/src/test/java/com/vince/backend/controller/GreetingControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,6 +20,13 @@ public class GreetingControllerTest {
     public void shouldReturnGreeting() throws Exception {
         mockMvc.perform(get("/api/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"greeting\":\"Hello Vincent\"}"));
+                .andExpect(jsonPath("$.greeting").value("Hello, Vincent"));
+    }
+
+    @Test
+    public void shouldReturnCustomizedGreeting() throws Exception {
+        mockMvc.perform(get("/api/hello?name=Deku"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.greeting").value("Hello, Deku"));
     }
 }
